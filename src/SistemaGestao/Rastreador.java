1af8945f1755b38407df2f56147573801e5c6a02
yurisawaki <141770852+yurisawaki@users.noreply.github.com>
2024-10-03 12:14:58 -0300

    Update Rastreador.java

--- src/SistemaGestao/Rastreador.java
+++ src/SistemaGestao/Rastreador.java
@@ -4,26 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class Rastreador {
-    private Map<Veiculo, String> veiculosMonitorados;
+public class Rastreador<T extends Veiculo> {
+    private Map<T, String> veiculosMonitorados;
 
     public Rastreador() {
         veiculosMonitorados = new HashMap<>();
     }
 
-    public void adicionarVeiculo(Veiculo veiculo) {
+    public void adicionarVeiculo(T veiculo) {
         veiculosMonitorados.put(veiculo, "Rastreando");
     }
 
-    public void monitorarEvento(Veiculo veiculo, EventoDesempenho evento) {
+    public void monitorarEvento(T veiculo, EventoDesempenho<? extends Veiculo> evento) {
         System.out.println("Monitorando evento para " + veiculo.getClass().getSimpleName() + ": " + evento.getDescricao());
     }
 
-    public void notificarMarco(Veiculo veiculo, String marco) {
+    public void notificarMarco(T veiculo, String marco) {
         System.out.println("Veículo " + veiculo.getClass().getSimpleName() + " atingiu o marco: " + marco);
     }
 
-    public void registrarEvento(Veiculo veiculo, EventoDesempenho evento) {
+    public void registrarEvento(T veiculo, EventoDesempenho<? extends Veiculo> evento) {
         monitorarEvento(veiculo, evento);
     }
 }
+
+
