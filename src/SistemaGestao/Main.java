fc3e14f4f3e94e17cf98f6b27f35b1ef90462b61
yurisawaki <141770852+yurisawaki@users.noreply.github.com>
2024-10-08 14:25:09 -0300

    Update Main.java

--- src/SistemaGestao/Main.java
+++ src/SistemaGestao/Main.java
@@ -2,48 +2,52 @@ package SistemaGestao;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Caminhao caminhao = new Caminhao("Toyota", "Supra", 2000, 100000);
+            Carro carro = new Carro("Ford", "Fusion", 2005, 50000);
+            Moto moto = new Moto("Honda", "Hornet", 2012, 10000);
 
-        Caminhao caminhao = new Caminhao("Toyota", "Supra", 2000, 100000);
-        Carro carro = new Carro("Ford", "Fusion", 2005, 50000);
-        Moto moto = new Moto("Honda", "Hornet", 2012, 10000);
+            System.out.println("Atributos dos veículos:");
+            System.out.println(caminhao);
+            System.out.println(carro);
+            System.out.println(moto);
 
-        System.out.println("Atributos dos veículos:");
-        System.out.println(caminhao);
+            Frota frota = new Frota();
+            frota.adicionarVeiculo(caminhao);
+            frota.adicionarVeiculo(carro);
+            frota.adicionarVeiculo(moto);
 
+            System.out.println("Realizando manutenção em todos os veículos:");
+            frota.realizarManutencaoEmTodos();
 
-        Frota frota = new Frota();
-        frota.adicionarVeiculo(caminhao);
-        frota.adicionarVeiculo(carro);
-        frota.adicionarVeiculo(moto);
+            Viagem<Caminhao> viagemCaminhao = new Viagem<>(caminhao, "Manaus", "Belém", 530);
+            Viagem<Carro> viagemCarro = new Viagem<>(carro, "Curitiba", "Porto Alegre", 700);
+            Viagem<Moto> viagemMoto = new Viagem<>(moto, "Florianópolis", "Blumenau", 150);
 
-        System.out.println("Realizando manutenção em todos os veículos:");
-        frota.realizarManutencaoEmTodos();
+            System.out.println("\nIniciando viagens:");
+            viagemCaminhao.iniciarViagem();
+            viagemCarro.iniciarViagem();
+            viagemMoto.iniciarViagem();
 
-        Viagem<Caminhao> viagemCaminhao = new Viagem<>(caminhao, "Manaus", "Belém", 530);
-        Viagem<Carro> viagemCarro = new Viagem<>(carro, "Curitiba", "Porto Alegre", 700);
-        Viagem<Moto> viagemMoto = new Viagem<>(moto, "Florianópolis", "Blumenau", 150);
+            Rastreador<Veiculo> rastreador = new Rastreador<>();
+            rastreador.adicionarVeiculo(caminhao);
+            rastreador.adicionarVeiculo(carro);
+            rastreador.adicionarVeiculo(moto);
 
-        System.out.println("\nIniciando viagens:");
-        viagemCaminhao.iniciarViagem();
-        viagemCarro.iniciarViagem();
-        viagemMoto.iniciarViagem();
+            EventoDesempenho<Veiculo> eventoCaminhao = new EventoDesempenho<>(caminhao, "Consumo de combustível: 40L/100km");
+            EventoDesempenho<Veiculo> eventoCarro = new EventoDesempenho<>(carro, "Consumo de combustível: 12L/100km");
+            EventoDesempenho<Veiculo> eventoMoto = new EventoDesempenho<>(moto, "Consumo de combustível: 6L/100km");
 
+            System.out.println("\nRegistrando eventos de desempenho:");
+            rastreador.registrarEvento(caminhao, eventoCaminhao);
+            rastreador.registrarEvento(carro, eventoCarro);
+            rastreador.registrarEvento(moto, eventoMoto);
 
-        Rastreador<Veiculo> rastreador = new Rastreador<>();
-        rastreador.adicionarVeiculo(caminhao);
-        rastreador.adicionarVeiculo(carro);
-        rastreador.adicionarVeiculo(moto);
-
-        EventoDesempenho<Veiculo> eventoCaminhao = new EventoDesempenho<>(caminhao, "Consumo de combustível: 40L/100km");
-        EventoDesempenho<Veiculo> eventoCarro = new EventoDesempenho<>(carro, "Consumo de combustível: 12L/100km");
-        EventoDesempenho<Veiculo> eventoMoto = new EventoDesempenho<>(moto, "Consumo de combustível: 6L/100km");
-
-        System.out.println("\nRegistrando eventos de desempenho:");
-        rastreador.registrarEvento(caminhao, eventoCaminhao);
-        rastreador.registrarEvento(carro, eventoCarro);
-        rastreador.registrarEvento(moto, eventoMoto);
-
-        System.out.println("\nGerando relatórios de desempenho:");
-        frota.gerarRelatorioDesempenho();
+            System.out.println("\nGerando relatórios de desempenho:");
+            frota.gerarRelatorioDesempenho();
+        } catch (Exception e) {
+            System.err.println("Ocorreu um erro: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
